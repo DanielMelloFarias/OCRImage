7e220dc046bd902ac0a9c52425ca8b66e234e16a
DanielMelloFarias <danielmellofarias@gmail.com>
2024-03-21 00:44:19 -0300

    update ocrimage.py

--- ocrimage.py
+++ ocrimage.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import google.generativeai as genai
 import streamlit as st
+from dotenv import load_dotenv
+import os
+
 import PIL
 
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 genai.configure(api_key = os.environ.get('KeyMaster'))
 
@@ -10,18 +15,25 @@ genai.configure(api_key = os.environ.get('KeyMaster'))
 
 
 
-file = st.file_uploader("Pick a file")
-# Abre a imagem
-imagem = PIL.Image.open(file)
+file = st.file_uploader("Escolha uma imagem...", type=['jpg', 'jpeg', 'png'])
 
-# Roda os Modelos 
-modelText = genai.GenerativeModel('gemini-pro')
-modelVision = genai.GenerativeModel('gemini-pro-vision')
+# Verifica se um arquivo foi enviado
+if file is not None:
+    # Mostra a imagem enviada
+    st.image(file, caption='Imagem Enviada', use_column_width=True)
 
-# Mostra as respostas
-responseImage = modelVision.generate_content(['Extraia Informações: - Nome, - CPF, - Unidade Escolar, - Ano/Série, - Turma, - Nome do Responsável e CPF, - Cidade e Data da ASsinatura e qual box foi marcada', imagem])
-#responseImage = modelVision.generate_content(['Extraia Informações relevantes', img])
-response = modelText.generate_content(['Ajuste o texto e me mostre de forma organizada', responseImage.text])
-print (responseImage.text)
-print ("#################")
-print(response.text)
+    # Abre a imagem
+    imagem = PIL.Image.open(file)
+
+    # Roda os Modelos 
+    modelText = genai.GenerativeModel('gemini-pro')
+    modelVision = genai.GenerativeModel('gemini-pro-vision')
+
+
+    # Mostra as respostas
+    responseImage = modelVision.generate_content(['Extraia Informações: - Nome, - CPF, - Unidade Escolar, - Ano/Série, - Turma, - Nome do Responsável e CPF, - Cidade e Data da ASsinatura e qual box foi marcada', imagem])
+    #responseImage = modelVision.generate_content(['Extraia Informações relevantes', img])
+    response = modelText.generate_content(['Ajuste o texto e me mostre de forma organizada', responseImage.text])
+    print (responseImage.text)
+    print ("#################")
+    print(response.text)
